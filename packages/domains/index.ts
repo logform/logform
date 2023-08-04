0605f3e869992c31b3868ac3d8fb62bfde2865c7
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-04 20:09:50 +0100

    changed to ENV

--- packages/domains/index.ts
+++ packages/domains/index.ts
@@ -1,11 +1,9 @@
 const APP_DOMAIN =
-  process.env.NEXT_PUBLIC_ENV === "local"
+  process.env.ENV === "local"
     ? "http://localhost:4400"
     : "https://app.logform.io";
 
 const HOME_DOMAIN =
-  process.env.NEXT_PUBLIC_ENV === "local"
-    ? "http://localhost:3300"
-    : "https://logform.io";
+  process.env.ENV === "local" ? "http://localhost:3300" : "https://logform.io";
 
 export { APP_DOMAIN, HOME_DOMAIN };
