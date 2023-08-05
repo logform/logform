6914c298eeb92c0d97ecbb8086e595a1e4295894
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-05 13:24:05 +0100

    updated redirects

--- logform.io/next.config.js
+++ logform.io/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/logform/logform",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
