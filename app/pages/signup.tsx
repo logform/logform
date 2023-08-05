d20394f520f8f2d77df7edd7a64eba6c66e87504
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-05 12:57:44 +0100

    changes field placeholder

--- app/pages/signup.tsx
+++ app/pages/signup.tsx
@@ -26,7 +26,7 @@ const Signup = () => {
       />
       <Input
         onChange={(e) => setEmail(e.target.value.trim().toLowerCase())}
-        placeholder="Your name"
+        placeholder="Just your first name"
         text="What's your name?"
         type="email"
       />
