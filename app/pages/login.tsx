4cffc083c918713adff24f2c988c91fa8c5330fd
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-05 12:07:43 +0100

    initial commit

---
+++ app/pages/login.tsx
@@ -0,0 +1,32 @@
+import Button from "@/components/auth/Button";
+import Input from "@/components/auth/Input";
+import AuthLayout from "@/layouts/AuthLayout";
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = () => {};
+
+  return (
+    <AuthLayout authType="login" onSubmit={handleLogin}>
+      <Input
+        onChange={(e) => setEmail(e.target.value.trim().toLowerCase())}
+        placeholder="example@mail.com"
+        text="What's your email?"
+        type="email"
+      />
+      <Input
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Shh...it's a secret"
+        text="What's your password?"
+        type="password"
+      />
+      <Button text="Login" disabled={loading} loading={loading} />
+    </AuthLayout>
+  );
+};
+
+export default Login;
