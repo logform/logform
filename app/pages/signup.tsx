c39f52912f40309cb8ddecdcf50cbe3257bf6fcc
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-05 12:50:38 +0100

    created signup page

---
+++ app/pages/signup.tsx
@@ -0,0 +1,50 @@
+import Button from "@/components/auth/Button";
+import Input from "@/components/auth/Input";
+import AuthLayout from "@/layouts/AuthLayout";
+import { useState } from "react";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSignup = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
+  return (
+    <AuthLayout authType="signup" onSubmit={handleSignup}>
+      <Input
+        onChange={(e) => setEmail(e.target.value.trim().toLowerCase())}
+        placeholder="example@mail.com"
+        text="What's your email?"
+        type="email"
+      />
+      <Input
+        onChange={(e) => setEmail(e.target.value.trim().toLowerCase())}
+        placeholder="Your name"
+        text="What's your name?"
+        type="email"
+      />
+      <Input
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Shh...it's a secret"
+        text="Choose a password"
+        type="password"
+      />
+      <Input
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="•••••••••••••••••••"
+        text="Enter your password again"
+        type="password"
+      />
+      <Button text="Signup" disabled={!email || !password} loading={loading} />
+    </AuthLayout>
+  );
+};
+
+export default Signup;
