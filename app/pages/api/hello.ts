8b877e51982f954f91fa78eb9411919ca91b4d83
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-05 10:46:06 +0100

    test db connection

--- app/pages/api/hello.ts
+++ app/pages/api/hello.ts
@@ -1,1 +1,16 @@
-import { prisma } from "@/db";
+import { prisma } from "@/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await prisma.users.create({
+    data: {
+      email: "akinkunmioye42@gmail.com",
+      firstName: "Akinkunmi",
+      googleID: "google",
+    },
+  });
+  res.send("User created");
+};
+
+export default allowMethods(["GET"])(handler);
