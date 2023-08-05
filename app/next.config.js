6b41469036bffcd8fcb75dd4d4d596f650c0355b
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-05 13:12:34 +0100

    setup page redirection

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/logform/logform",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
