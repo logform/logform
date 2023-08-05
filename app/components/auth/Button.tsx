7e3755706f8b55042623f2f7303bc3ed6ceeb7ae
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-05 12:45:16 +0100

    render loading spinner if loading is true

--- app/components/auth/Button.tsx
+++ app/components/auth/Button.tsx
@@ -1,3 +1,5 @@
+import { CgSpinner } from "react-icons/cg";
+
 const Button = ({
   text,
   disabled,
@@ -9,10 +11,14 @@ const Button = ({
 }) => {
   return (
     <button
-      className="outline-none border-2 text-white bg-black/90 hover:bg-black transition-colors rounded-full pl-3 w-[450px] py-3 font-semibold text-center mt-5"
-      disabled={disabled}
+      className="outline-none border-2 text-white bg-black/90 hover:bg-black transition-colors rounded-full pl-3 w-[450px] py-3 font-semibold text-center mt-5 flex items-center justify-center disabled:cursor-not-allowed h-12"
+      disabled={disabled || loading}
     >
-      {loading ? <></> : text}
+      {loading ? (
+        <CgSpinner className="text-lg text-center animate-spin" />
+      ) : (
+        text
+      )}
     </button>
   );
 };
