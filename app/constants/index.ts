ee59805b151b9390dbc932cb596ad01185249b0a
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-05 13:21:01 +0100

    updated domains

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,11 +1,11 @@
 const APP_DOMAIN =
   process.env.NEXT_PUBLIC_ENV === "local"
     ? "http://localhost:4400"
-    : "https://app.logform.io";
+    : "https://app-logform.vercel.app";
 
 const HOME_DOMAIN =
   process.env.NEXT_PUBLIC_ENV === "local"
     ? "http://localhost:3300"
-    : "https://logform.io";
+    : "https://logform.vercel.app";
 
 export { APP_DOMAIN, HOME_DOMAIN };
