62d55b987453daaa59636044b3c937bc384e6d7e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-06 01:09:27 +0100

    send users verification email

--- app/pages/api/auth/signup.ts
+++ app/pages/api/auth/signup.ts
@@ -1,8 +1,11 @@
+import { plunk } from "@/configs/plunk";
+import { APP_DOMAIN } from "@/constants";
 import { prisma } from "@/prisma";
 import dayjs from "dayjs";
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 import validator from "validator";
+import bcrypt from "bcrypt";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, name, password, repeatPassword } = req.body;
@@ -56,11 +59,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       data: {
         email,
         name,
-        password,
+        password: bcrypt.hashSync(password, 10),
       },
     });
-
     const token = Math.random().toString(36).substring(2);
+    const verficationLink = `${APP_DOMAIN}/verify?token=${token}`;
 
     await prisma.verificationToken.create({
       data: {
@@ -69,9 +72,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         token,
       },
     });
+
+    await plunk.emails.send({
+      to: email,
+      subject: "Verify your email",
+      body: `
+        <p>Hello ${name},</p>
+        <p>Please click the button below to verify your email address:</p>
+        <a href="${verficationLink}" target="_blank" style="background-color: #0070f3; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Verify</a>
+      `,
+    });
+    res.status(200).json({ message: "Verification email sent" });
   } catch (error) {
     res.status(500).json({ error });
   }
 };
 
-export default allowMethods(["GET"])(handler);
+export default allowMethods(["POST"])(handler);
