eaf5862b9e034196d6407322e0e748d6cd4a04e5
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-06 10:54:36 +0100

    inital commit

---
+++ app/pages/api/auth/resend.ts
@@ -0,0 +1,59 @@
+import { plunk } from "@/configs/plunk";
+import { APP_DOMAIN } from "@/constants";
+import { prisma } from "@/prisma";
+import dayjs from "dayjs";
+import { NextApiRequest, NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const tkn = req.query.tkn;
+  if (!tkn) {
+    res.status(400).json({
+      message: "Token is missing in request",
+    });
+    return;
+  }
+  const token = await prisma.verificationToken.findUnique({
+    where: {
+      token: tkn as string,
+    },
+  });
+
+  if (!token) {
+    res.status(400).json({
+      message: "Invalid token",
+    });
+    return;
+  }
+
+  const user = await prisma.users.findUnique({
+    where: {
+      id: token.userId,
+    },
+  });
+
+  const linkToken = Math.random().toString(36).substring(2);
+  const verficationLink = `${APP_DOMAIN}/api/auth/verify?token=${linkToken}`;
+
+  await prisma.verificationToken.update({
+    where: {
+      token: tkn as string,
+    },
+    data: {
+      token: linkToken,
+      expires: dayjs(new Date()).add(14, "minutes").toDate(),
+    },
+  });
+
+  await plunk.emails.send({
+    to: user?.email as string,
+    subject: "Verify your email",
+    body: `
+        <p>Hello ${name},</p>
+        <p>Please click the button below to verify your email address:</p>
+        <a href="${verficationLink}" target="_blank" style="background-color: #0070f3; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Verify</a>
+      `,
+  });
+};
+
+export default allowMethods(["POST"])(handler);
