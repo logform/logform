47f12bdb42d5851a5435a31d6b280faf237d5836
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-06 10:55:18 +0100

    verify token

--- app/pages/resend.tsx
+++ app/pages/resend.tsx
@@ -1,9 +1,13 @@
 import Button from "@/components/auth/Button";
 import AuthLayout from "@/layouts/AuthLayout";
-import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Resend = () => {
   const [loading, setLoading] = useState(false);
+  const [isPageLoading, setIsPageLoading] = useState(true);
+  const [isTokenValid, setIsTokenValid] = useState(false);
 
   const handleResend = () => {
     setLoading(true);
@@ -11,15 +15,52 @@ const Resend = () => {
       setLoading(false);
     }, 3000);
   };
+
+  const router = useRouter();
+  useEffect(() => {
+    if (router.query.token) {
+      (async () => {
+        try {
+          await axios.get(`/api/auth/check-token?tkn=${router.query.token}`);
+          setIsPageLoading(false);
+          setIsTokenValid(true);
+        } catch (error: any) {
+          setIsPageLoading(false);
+        }
+      })();
+    }
+  }, [router.query.token]);
+
   return (
-    <AuthLayout authType="resend" onSubmit={handleResend}>
-      <div className="flex items-center justify-center flex-col">
-        <p className="text-center text-lg font-medium text-gray-500">
-          Click the button below to request a new verification email
-        </p>
-        <Button text="Resend" loading={loading} disabled={loading} />
-      </div>
-    </AuthLayout>
+    <>
+      {isPageLoading ? (
+        <div className="h-screen flex items-center justify-center flex-col">
+          <h1 className="animate-pulse font-bold text-5xl text-center">
+            Logform
+          </h1>
+          {/* <div className="w-52 h-1 bg-black/20 mt-4">
+            <div className={`w-[${width}%] h-full bg-black`}></div>
+          </div> */}
+        </div>
+      ) : (
+        <>
+          {isTokenValid ? (
+            <AuthLayout authType="resend" onSubmit={handleResend}>
+              <div className="flex items-center justify-center flex-col">
+                <p className="text-center text-lg font-medium text-gray-500">
+                  Click the button below to request a new verification email
+                </p>
+                <Button text="Resend" loading={loading} disabled={loading} />
+              </div>
+            </AuthLayout>
+          ) : (
+            <div className="h-screen flex items-center justify-center flex-col">
+              <h1>Invalid verification token</h1>
+            </div>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
