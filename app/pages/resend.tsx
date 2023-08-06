c600974228699e043e79ad2c4517adb7f1509e65
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-06 09:20:19 +0100

    updated styling and handleResned function

--- app/pages/resend.tsx
+++ app/pages/resend.tsx
@@ -1,12 +1,24 @@
+import Button from "@/components/auth/Button";
 import AuthLayout from "@/layouts/AuthLayout";
+import { useState } from "react";
 
 const Resend = () => {
-  const handleResend = () => {};
+  const [loading, setLoading] = useState(false);
+
+  const handleResend = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
   return (
     <AuthLayout authType="resend" onSubmit={handleResend}>
-      <p className="text-center text-lg font-medium text-gray-500">
-        Click the button below to request a new verification email
-      </p>
+      <div className="flex items-center justify-center flex-col">
+        <p className="text-center text-lg font-medium text-gray-500">
+          Click the button below to request a new verification email
+        </p>
+        <Button text="Resend" loading={loading} disabled={loading} />
+      </div>
     </AuthLayout>
   );
 };
