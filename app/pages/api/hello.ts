ff0c15e9827dcc22cd602b77df69b53b2730e952
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-07 22:02:27 +0100

    test cookies

--- app/pages/api/hello.ts
+++ app/pages/api/hello.ts
@@ -1,16 +1,11 @@
 import { prisma } from "@/prisma";
+import { setAuthCookies } from "@/utils/setAuthCookies";
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await prisma.users.create({
-    data: {
-      email: "akinkunmioye42@gmail.com",
-      firstName: "Akinkunmi",
-      googleID: "google",
-    },
-  });
-  res.send("User created");
+  await setAuthCookies(req, res, "ps082bdvdjdjjdid73j");
+  res.send("ok");
 };
 
 export default allowMethods(["GET"])(handler);
