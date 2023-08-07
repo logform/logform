108bcc94382d5cd8474cd2ae01f18bcd7b7fdf98
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-07 05:27:25 +0100

    store refresh token in db

--- app/utils/setAuthCookies.ts
+++ app/utils/setAuthCookies.ts
@@ -1,13 +1,22 @@
+import { prisma } from "@/prisma";
+import dayjs from "dayjs";
 import { sign } from "jsonwebtoken";
 import { NextApiResponse } from "next";
 
-export const authenticate = (userId: string, res: NextApiResponse) => {
+export const setAuthCookies = async (userId: string, res: NextApiResponse) => {
   const accessToken = sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
     expiresIn: "15m",
   });
 
   const refreshToken = sign({ userId }, process.env.REFRESH_TOKEN_SECRET!, {
     expiresIn: "90d",
+  });
+
+  await prisma.refreshTokens.create({
+    data: {
+      token: refreshToken,
+      expires: dayjs().add(90, "days").toDate(),
+    },
   });
 
   res.setHeader("Set-Cookie", [
