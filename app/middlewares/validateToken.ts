50ced99f4cacea41f9a2479668723bf94bd8cff2
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-07 22:02:13 +0100

    initial commit

---
+++ app/middlewares/validateToken.ts
@@ -0,0 +1,38 @@
+import { NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import { prisma } from "@/prisma";
+import { ExtendedRequest } from "@/interfaces";
+import { getCookie } from "cookies-next";
+import dayjs from "dayjs";
+
+export const validateToken = async (
+  req: ExtendedRequest,
+  res: NextApiResponse,
+  next: () => void
+) => {
+  const token = getCookie("refresh-token", {
+    req,
+    res,
+  }) as string;
+
+  const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET!) as {
+    userId: string;
+  };
+
+  const tokenData = await prisma.refreshTokens.findUnique({
+    where: {
+      userId: decoded?.userId,
+    },
+  });
+
+  if (!tokenData) {
+    return res.status(400).json({ message: "Invalid token" });
+  }
+
+  const expired = dayjs(tokenData.expires).isAfter(new Date());
+
+  if (!expired) return res.status(400).json({ message: "Invalid token" });
+
+  req.userId = decoded?.userId;
+  next();
+};
