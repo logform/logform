b66ef16b22155316d12529fb56c0a92c735153c3
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-07 22:02:41 +0100

    initial commit

---
+++ app/pages/api/auth/refresh-token.ts
@@ -0,0 +1,35 @@
+import { ExtendedRequest } from "@/interfaces";
+import { NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+import { validateToken } from "@/middlewares/validateToken";
+import { setCookie } from "cookies-next";
+import dayjs from "dayjs";
+import { sign } from "jsonwebtoken";
+import { APP_DOMAIN } from "@/constants";
+
+const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
+  const isProd = process.env.NODE_ENV === "production";
+
+  const accessToken = sign(
+    { userId: req.userId },
+    process.env.ACCESS_TOKEN_SECRET!,
+    {}
+  );
+
+  setCookie("access-token", accessToken, {
+    req,
+    res,
+    sameSite: isProd ? "none" : "lax",
+    expires: dayjs().add(15, "minutes").toDate(),
+    httpOnly: true,
+    secure: isProd,
+    domain: isProd ? APP_DOMAIN : "localhost",
+  });
+
+  res.send("ok");
+};
+
+export default allowMethods(["GET"])(
+  (req: ExtendedRequest, res: NextApiResponse) =>
+    validateToken(req, res, () => handler(req, res))
+);
