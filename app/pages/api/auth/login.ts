ad2f7f2c9c712daf52a5da53bea537f3a875d0b6
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-07 22:36:34 +0100

    initial commit

---
+++ app/pages/api/auth/login.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+import bcrypt from "bcrypt";
+import { prisma } from "@/prisma";
+import { setAuthCookies } from "@/utils/setAuthCookies";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ message: "Enter your email and password." });
+    return;
+  }
+
+  const user = await prisma.users.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    res.status(400).json({ message: "Email or password is incorrect." });
+    return;
+  }
+
+  const doesPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!doesPasswordMatch) {
+    res.status(400).json({ message: "Email or password is incorrect." });
+    return;
+  }
+
+  await setAuthCookies(req, res, user.id);
+};
+export default allowMethods(["POST"])(handler);
