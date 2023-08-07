ee6d42d228f24bb8604a01a98a6e376c9fc8e37e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-07 22:03:28 +0100

    set cookies

--- app/utils/setAuthCookies.ts
+++ app/utils/setAuthCookies.ts
@@ -1,26 +1,49 @@
 import { prisma } from "@/prisma";
 import dayjs from "dayjs";
 import { sign } from "jsonwebtoken";
-import { NextApiResponse } from "next";
+import { setCookie } from "cookies-next";
+import { APP_DOMAIN } from "@/constants";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export const setAuthCookies = async (userId: string, res: NextApiResponse) => {
-  const accessToken = sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
-    expiresIn: "15m",
-  });
+export const setAuthCookies = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  userId: string
+) => {
+  try {
+    const isProd = process.env.NODE_ENV === "production";
 
-  const refreshToken = sign({ userId }, process.env.REFRESH_TOKEN_SECRET!, {
-    expiresIn: "90d",
-  });
+    const accessToken = sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
+      expiresIn: "15m",
+    });
 
-  await prisma.refreshTokens.create({
-    data: {
-      token: refreshToken,
-      expires: dayjs().add(90, "days").toDate(),
-    },
-  });
+    const refreshToken = sign({ userId }, process.env.REFRESH_TOKEN_SECRET!, {
+      expiresIn: "90d",
+    });
 
-  res.setHeader("Set-Cookie", [
-    `access-token=${accessToken}; HttpOnly; SameSite=Strict; Path=/`,
-    `refresh-token=${refreshToken}; HttpOnly; SameSite=Strict; Path=/`,
-  ]);
+    await prisma.refreshTokens.create({
+      data: {
+        userId,
+        token: refreshToken,
+        expires: dayjs().add(90, "days").toDate(),
+      },
+    });
+
+    const SetCookie = (key: string, value: string, expires: Date) => {
+      setCookie(key, value, {
+        req,
+        res,
+        sameSite: isProd ? "none" : "lax",
+        expires,
+        httpOnly: true,
+        secure: isProd,
+        domain: isProd ? APP_DOMAIN : "localhost",
+      });
+    };
+
+    SetCookie("access-token", accessToken, dayjs().add(15, "minutes").toDate());
+    SetCookie("refresh-token", refreshToken, dayjs().add(90, "days").toDate());
+  } catch (error) {
+    console.log(error);
+  }
 };
