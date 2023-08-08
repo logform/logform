c43cf41c6792bc1151909d8197dd57e18842c0e6
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-08 22:13:48 +0100

    updated name

--- app/pages/api/auth/complete.ts
+++ app/pages/api/auth/complete.ts
@@ -6,22 +6,33 @@ import { NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
 const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
+  const { name } = req.body as { name: string };
+
+  if (!name) return res.status(400).json({ message: "Name is required" });
+
+  if (name.includes(" "))
+    return res.status(400).json({ message: "Name cannot contain spaces" });
+
+  if (name.length < 2)
+    return res
+      .status(400)
+      .json({ message: "Name must be at least 2 characters long" });
+
   await prisma.users.update({
     where: {
       id: req.userId,
     },
     data: {
+      name,
       hasCompletedSetup: true,
     },
   });
   await setAuthCookies(req, res, req.userId);
 
-  res.send("ok");
+  res.status(200).json({ message: "Successfully updated name" });
 };
 
-const middlewareChain = allowMethods(["GET"])(
+export default allowMethods(["PUT"])(
   (req: ExtendedRequest, res: NextApiResponse) =>
     validateAccessToken(req, res, () => handler(req, res))
 );
-
-export default middlewareChain;
