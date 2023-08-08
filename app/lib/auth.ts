7490669562a41cc2dae3b3edb3321f3b6b7a6519
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-08 12:00:15 +0100

    created verifyAuth function

---
+++ app/lib/auth.ts
@@ -0,0 +1,21 @@
+import { jwtVerify } from "jose";
+
+export const getJwtSecret = () => {
+  const secret = process.env.REFRESH_TOKEN_SECRET;
+  if (!secret) {
+    throw new Error("Refresh token secret not found");
+  }
+  return secret;
+};
+
+export const verifyAuth = async (token: string) => {
+  try {
+    const verified = await jwtVerify(
+      token,
+      new TextEncoder().encode(getJwtSecret())
+    );
+    return verified.payload as { userId: string };
+  } catch (error) {
+    throw new Error("Invalid token");
+  }
+};
