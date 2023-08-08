df79482b77871b171053c91ab7765c75fe96b2db
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-09 00:36:03 +0100

    refresh token every 14 minutes

--- app/layouts/DashboardLayout.tsx
+++ app/layouts/DashboardLayout.tsx
@@ -1,6 +1,18 @@
+import axios from "axios";
+import dayjs from "dayjs";
 import { ReactNode } from "react";
+import useInterval from "use-interval";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  useInterval(async () => {
+    try {
+      const { data } = await axios("/api/auth/refresh-token");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, 840000);
+
   return <div>{children}</div>;
 };
 
