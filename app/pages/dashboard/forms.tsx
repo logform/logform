12204ece06002d0042e21409c86a2440d5e067a5
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-09 03:51:15 +0100

    updated component

--- app/pages/dashboard/forms.tsx
+++ app/pages/dashboard/forms.tsx
@@ -1,6 +1,38 @@
+import FormCard from "@/components/dashboard/FormCard";
 import DashboardLayout from "@/layouts/DashboardLayout";
+import Link from "next/link";
+import { BiPlus } from "react-icons/bi";
+
 const Forms = () => {
-  return <DashboardLayout>Dashboard</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <div className="flex items-center gap-5 flex-wrap">
+        <div className="w-[225px] h-[250px] rounded-md border-2 border-black/10 hover:border-black/40 cursor-pointer transition-all ease-in-out duration-300 flex items-center flex-col justify-center hover:bg-black/[3%]">
+          <BiPlus className="text-5xl" />
+          <span className="font-semibold">Create a logform</span>
+        </div>
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+        <FormCard />
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Forms;
