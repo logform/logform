63d71f1aa4056645f910b589dc3c49620820d7f9
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-09 16:11:24 +0100

    add new types and interfaces

--- app/interfaces.ts
+++ app/interfaces.ts
@@ -1,6 +1,29 @@
 import type { NextApiRequest } from "next";
+import type { ReactNode } from "react";
 
 export interface ExtendedRequest extends NextApiRequest {
   userId: string;
   expired: Boolean;
+}
+
+export type FieldTypes =
+  | "short-text"
+  | "long-text"
+  | "multiple-choice"
+  | "email"
+  | "picture-choice"
+  | "file-upload";
+
+export interface FieldTypeProps {
+  icon: ReactNode;
+  text: string;
+  color: string;
+  type: FieldTypes;
+}
+
+export interface QuestionProps {
+  index: number;
+  label: string;
+  type: FieldTypes;
+  required: boolean;
 }
