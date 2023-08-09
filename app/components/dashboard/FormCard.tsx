acbaeb65a2cc511ea27391714b3ce3d4aa1edb24
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-09 03:50:46 +0100

    initial commit

---
+++ app/components/dashboard/FormCard.tsx
@@ -0,0 +1,9 @@
+const FormCard = () => {
+  return (
+    <div className="w-[225px] h-[250px] rounded-md border-2 border-black/10 hover:border-black/40 cursor-pointer transition-all ease-in-out duration-300 flex items-center flex-col justify-center hover:bg-black/[3%]">
+      FormCard
+    </div>
+  );
+};
+
+export default FormCard;
