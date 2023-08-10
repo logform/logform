b9ec660331a81fa991d6b844c83214059a12698f
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-10 06:38:50 +0100

    moved refresh function to a new component

--- app/layouts/DashboardLayout.tsx
+++ app/layouts/DashboardLayout.tsx
@@ -1,26 +1,13 @@
 import Navbar from "@/components/dashboard/Navbar";
-import axios from "axios";
+import RefreshProvider from "@/providers/RefreshProvider";
 import { ReactNode } from "react";
-import useInterval from "use-interval";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
-  useInterval(
-    async () => {
-      try {
-        await axios("/api/auth/refresh-token");
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    840000,
-    true
-  );
-
   return (
-    <div className="">
+    <RefreshProvider>
       <Navbar />
       <div className="px-16 mt-10">{children}</div>
-    </div>
+    </RefreshProvider>
   );
 };
 
