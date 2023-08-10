1133071cef35dd302773b5fcd035470baa32ee3c
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-10 04:54:44 +0100

    fixed defaultValue

--- app/components/dashboard/form/field-types/ShortText/Settings.tsx
+++ app/components/dashboard/form/field-types/ShortText/Settings.tsx
@@ -16,6 +16,7 @@ const ShortTextSettings = ({
   onChangeEnforceMaxCharacters?: () => void;
   onChangeMaxCharacters?: (e: ChangeEvent<HTMLInputElement>) => void;
   maxCharacters?: number;
+  defaultValue?: number;
 }) => {
   const isMaxCharacterEnforced =
     enforceMaxCharacters || maxCharacters !== undefined;
@@ -41,7 +42,7 @@ const ShortTextSettings = ({
           className="w-full border-2 border-gray-200 font-lh rounded-md pl-3 py-2"
           placeholder="0 - 999,999,999"
           onChange={(e) => onChangeMaxCharacters && onChangeMaxCharacters(e)}
-          defaultValue={isMaxCharacterEnforced && maxCharacters}
+          value={isMaxCharacterEnforced && maxCharacters}
         />
       )}
     </>
