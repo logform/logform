2f650893cf7e045c956c3e00425720dc769e9a27
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-11 11:45:13 +0100

    style input

--- app/components/dashboard/form/field-types/MultipleChoice/index.tsx
+++ app/components/dashboard/form/field-types/MultipleChoice/index.tsx
@@ -1,15 +1,27 @@
 const MultipleChoice = ({
   options,
   onClick,
+  onChange,
 }: {
   options: string[];
   onClick: () => void;
+  onChange?: () => void;
 }) => {
   return (
     <div>
-      {options.length}
       {options.map((option, index) => (
-        <input key={index} value={option} />
+        <div
+          className="border-2 border-gray-300 pl-3 rounded-full px-2 flex items-center justify-between my-2 group focus:border-gray-400 group"
+          key={index}
+        >
+          <small>{index + 1}.</small>
+          <input
+            defaultValue={option}
+            onChange={onChange}
+            className="pl-3 py-2 rounded-full outline-none"
+          />
+          <button className="opacity-0 group-hover:opacity-100">x</button>
+        </div>
       ))}
       <button onClick={onClick}>Add a new option</button>
     </div>
