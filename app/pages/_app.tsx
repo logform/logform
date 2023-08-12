2c7cfbb117853168dc278cfc6c5b939d66cb6116
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-12 08:15:10 +0100

    rendered progress bar

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,10 +2,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import NextNProgress from "nextjs-progressbar";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextNProgress color="#000" />
       <ToastContainer />
       <Component {...pageProps} />
     </>
