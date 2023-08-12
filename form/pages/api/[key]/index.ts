1bfb2d3489b5a83a41cf7c7b7bd593aefdf64170
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-12 18:12:28 +0100

    ??

---
+++ form/pages/api/[key]/index.ts
@@ -0,0 +1,83 @@
+import { prisma } from "@/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const key = req.query.key as string;
+  try {
+    const form = await prisma.forms.findUnique({
+      where: {
+        key,
+      },
+      include: {
+        shortTextFields: {
+          orderBy: {
+            index: "asc",
+          },
+        },
+        longTextsFields: {
+          orderBy: {
+            index: "asc",
+          },
+        },
+        MultipleChoiceFields: {
+          include: {
+            options: true,
+          },
+          orderBy: {
+            index: "asc",
+          },
+        },
+      },
+    });
+
+    if (!form) {
+      return res.status(404).json({ error: "Form not found" });
+    }
+
+    const formatted = {
+      questions: form.shortTextFields
+        .map((field) => ({
+          index: field.index,
+          type: "short-text",
+          label: field.label,
+          required: field.required,
+        }))
+        .concat(
+          form.MultipleChoiceFields.map((field) => ({
+            index: field.index,
+            label: field.label,
+            required: field.required,
+            type: "multiple-choice",
+            options: field.options.map((option) => ({
+              index: option.index,
+              value: option.value,
+            })),
+          }))
+        )
+        .concat(
+          form.longTextsFields.map((field) => ({
+            index: field.index,
+            type: "long-text",
+            label: field.label,
+            required: field.required,
+          }))
+        ),
+    };
+
+    const sorted = formatted.questions.sort((a, b) => a.index - b.index);
+
+    const response = {
+      title: form.title,
+      key: form.key,
+      questions: sorted,
+    };
+
+    res.json(response);
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+export default allowMethods(["GET"])(handler);
