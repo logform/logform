4c5a968bf51794b7a867406932e93b2148f69c02
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-12 17:21:33 +0100

    get form

--- form/pages/api/[form]/index.ts
+++ form/pages/api/[form]/index.ts
@@ -1,6 +1,30 @@
+import { prisma } from "@/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {};
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const key = req.query.form as string;
+  try {
+    const form = await prisma.forms.findUnique({
+      where: {
+        key,
+      },
+      include: {
+        longTextsFields: true,
+        MultipleChoiceFields: true,
+        shortTextFields: true,
+      },
+    });
+
+    if (!form) {
+      return res.status(404).json({ error: "Form not found" });
+    }
+
+    res.json(form);
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
 
 export default allowMethods(["GET"])(handler);
