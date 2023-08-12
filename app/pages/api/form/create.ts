4e05ebb38256614b572e2784f1c6257e613c8c9e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-12 16:08:29 +0100

    wrapped in try catch block

--- app/pages/api/form/create.ts
+++ app/pages/api/form/create.ts
@@ -25,61 +25,67 @@ const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
     questions,
   };
 
-  const newForm = await prisma.forms.create({
-    data: {
-      title: form.title,
-      userId: req.userId,
-      key: generateKey(6),
-    },
-  });
+  try {
+    const newForm = await prisma.forms.create({
+      data: {
+        title: form.title,
+        userId: req.userId,
+        key: generateKey(6),
+      },
+    });
 
-  form.questions.map(async (question) => {
-    switch (question.type) {
-      case "short-text":
-        await prisma.shortTextsFields.create({
-          data: {
-            formId: newForm.id,
-            label: question.label,
-            index: question.index,
-            required: question.required,
-            maxCharacters: question?.maxCharacters,
-          },
-        });
-        break;
-      case "long-text":
-        await prisma.longTextsFields.create({
-          data: {
-            formId: newForm.id,
-            index: question.index,
-            label: question.label,
-            required: question.required,
-            maxCharacters: question?.maxCharacters,
-          },
-        });
-        break;
-      case "multiple-choice":
-        const newMultipleChoice = await prisma.multipleChoiceFields.create({
-          data: {
-            formId: newForm.id,
-            index: question.index,
-            label: question.label,
-            required: question.required,
-          },
-        });
-        question.options.map(async (option) => {
-          await prisma.multipleChoiceFieldsOptions.create({
+    form.questions.map(async (question) => {
+      switch (question.type) {
+        case "short-text":
+          await prisma.shortTextsFields.create({
             data: {
-              index: option.index,
-              value: option.value,
-              questionId: newMultipleChoice.id,
+              formId: newForm.id,
+              label: question.label,
+              index: question.index,
+              required: question.required,
+              maxCharacters: question?.maxCharacters,
             },
           });
-        });
-        break;
-      default:
-        break;
-    }
-  });
+          break;
+        case "long-text":
+          await prisma.longTextsFields.create({
+            data: {
+              formId: newForm.id,
+              index: question.index,
+              label: question.label,
+              required: question.required,
+              maxCharacters: question?.maxCharacters,
+            },
+          });
+          break;
+        case "multiple-choice":
+          const newMultipleChoice = await prisma.multipleChoiceFields.create({
+            data: {
+              formId: newForm.id,
+              index: question.index,
+              label: question.label,
+              required: question.required,
+            },
+          });
+          question.options.map(async (option) => {
+            await prisma.multipleChoiceFieldsOptions.create({
+              data: {
+                index: option.index,
+                value: option.value,
+                questionId: newMultipleChoice.id,
+              },
+            });
+          });
+          break;
+        default:
+          break;
+      }
+    });
+    res.send("Form created");
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }
 };
 
 export default allowMethods(["GET"])(
