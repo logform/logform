620cde5156cbb78125aebef708ceaf4141b1befa
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-12 18:23:14 +0100

    removed React import

--- form/components/LongText.tsx
+++ form/components/LongText.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const LongText = () => {
   return <div>LongText</div>;
 };
--- form/components/MultipleChoice.tsx
+++ form/components/MultipleChoice.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const MultipleChoice = () => {
   return <div>MultipleChoice</div>;
 };
