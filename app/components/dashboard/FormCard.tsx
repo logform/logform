cee88200658d668819ecc0e63b8d86128638b97a
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-13 13:42:19 +0100

    added title props

--- app/components/dashboard/FormCard.tsx
+++ app/components/dashboard/FormCard.tsx
@@ -3,10 +3,12 @@ import { BsQuestionLg } from "react-icons/bs";
 import { HiEye } from "react-icons/hi2";
 
 const FormCard = ({
+  title,
   questions,
   impressions,
   submissions,
 }: {
+  title: string;
   questions: number;
   impressions: number;
   submissions: number;
@@ -32,7 +34,7 @@ const FormCard = ({
   return (
     <div className="w-[225px] h-[250px] rounded-md border-2 p-2 border-black/10 hover:border-black/40 cursor-pointer transition-all ease-in-out duration-300 flex items-center flex-col justify-between hover:bg-black/[3%]">
       <div className=""></div>
-      <p className="text-2xl font-bold">My Form</p>
+      <p className="text-2xl font-bold">{title}</p>
       <div className="w-full">
         {stats.map((stat, i) => (
           <div
