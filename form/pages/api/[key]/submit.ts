66598116c611ec4b424a416de9076dae745bb9d8
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-13 18:21:07 +0100

    ??

--- form/pages/api/[key]/submit.ts
+++ form/pages/api/[key]/submit.ts
@@ -8,22 +8,22 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const submissionData: SubmissionProps[] = [
     {
-      type: "short-text",
+      type: "SHORT_TEXT",
       questionId: "e95583cd-2d54-4eeb-843e-ca3c3ca1018b",
-      answer: "Akinkunmi",
+      value: "Akinkunmi",
       submissionId: "",
     },
     {
-      type: "long-text",
+      type: "LONG_TEXT",
       questionId: "1311370d-996e-489b-a024-545cc30eb482",
-      answer:
+      value:
         "Software engineer with a passion for building software that improves the lives of those around it. I enjoy solving problems and building products that have a positive impact on people.",
       submissionId: "",
     },
     {
-      type: "multiple-choice",
+      type: "MULTIPLE_CHOICE",
       questionId: "da51189a-8600-42d1-8624-dd3b62a95dab",
-      answer: ["Air", "Water"],
+      multipleChoiceValue: ["Air", "Water"],
       submissionId: "",
     },
   ];
@@ -48,37 +48,35 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       },
     });
 
-    for (const answer of submissionData) {
-      switch (answer.type) {
-        case "short-text":
-          await prisma.shortTextSubmissions.create({
-            data: {
-              questionId: answer.questionId,
-              submissionId: newSubmission.id,
-              answer: answer.answer,
-            },
-          });
-          break;
-        case "long-text":
-          await prisma.longTextSubmissions.create({
-            data: {
-              questionId: answer.questionId,
-              submissionId: newSubmission.id,
-              answer: answer.answer,
-            },
-          });
-          break;
-        case "multiple-choice":
-          await prisma.multipleChoiceSubmissions.create({
-            data: {
-              questionId: answer.questionId,
-              submissionId: newSubmission.id,
-              answer: answer.answer as string[],
-            },
-          });
-          break;
-      }
-    }
+    const questionTypeHandlers = {
+      SHORT_TEXT: (submission: SubmissionProps) => {
+        await prisma.answers.create({
+          data: {
+            submissionId: newSubmission.id,
+            questionId: submission.questionId,
+            value: submission.,
+          },
+        });
+      },
+      LONG_TEXT: (submission: SubmissionProps) => {
+        // Handle LONG_TEXT submission logic
+      },
+      MULTIPLE_CHOICE: (submission: SubmissionProps) => {
+        // Handle MULTIPLE_CHOICE submission logic
+      },
+      // Add handlers for other question types here
+    };
+
+    await prisma.answers.createMany({
+      data: submissionData.map((submission) => {
+        const handler = questionTypeHandlers[submission.type];
+        if (handler) {
+          return handler(submission);
+        }
+        return submission;
+      }),
+    });
+
     res.send("Submission sent");
   } catch (error) {
     console.log(error);
