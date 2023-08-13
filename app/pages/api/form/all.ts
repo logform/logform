2acb6ff7550ed1cf6179f8e29ecca77d4a6e3726
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-13 12:47:21 +0100

    get forms

--- app/pages/api/form/all.ts
+++ app/pages/api/form/all.ts
@@ -5,14 +5,22 @@ import { NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
 const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
-  const forms = await prisma.forms.findMany({
-    include: {
-      submissions: true,
-      questions: true,
-    },
-  });
-
-  res.status(200).json(forms);
+  try {
+    const forms = await prisma.forms.findMany({
+      where: {
+        userId: req.userId,
+      },
+      select: {
+        title: true,
+        key: true,
+        _count: true,
+      },
+    });
+    res.json(forms);
+  } catch (error) {
+    res.status(500).json({ error });
+    console.error(error);
+  }
 };
 
 export default allowMethods(["GET"])(
