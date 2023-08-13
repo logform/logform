4d133f6775611e6e209e82f9b2bb98ac3e0185ae
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-13 09:38:41 +0100

    ??

---
+++ app/pages/api/form/all.ts
@@ -0,0 +1,20 @@
+import { ExtendedRequest } from "@/interfaces";
+import { validateAccessToken } from "@/middlewares/validateAccessToken";
+import { prisma } from "@/prisma";
+import { NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+
+const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
+  const forms = await prisma.forms.findMany({
+    include: {
+      submissions: true,
+    },
+  });
+
+  res.status(200).json(forms);
+};
+
+export default allowMethods(["GET"])(
+  (req: ExtendedRequest, res: NextApiResponse) =>
+    validateAccessToken(req, res, () => handler(req, res))
+);
