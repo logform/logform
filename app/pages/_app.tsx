cbc8e03468b742e34c91d1ed9d4c6b4dc2bdb6f7
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-15 13:07:44 +0100

    global

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,12 +3,13 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import NextNProgress from "nextjs-progressbar";
+import TokenRefresher from "@/components/TokenRefresher";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <TokenRefresher>
       <NextNProgress color="#000" />
       <ToastContainer />
       <Component {...pageProps} />
-    </>
+    </TokenRefresher>
   );
 }
