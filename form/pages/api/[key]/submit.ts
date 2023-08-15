06eb87259ec1ef2151c1d201734f3c051862e659
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-15 12:05:27 +0100

    submitt

--- form/pages/api/[key]/submit.ts
+++ form/pages/api/[key]/submit.ts
@@ -1,91 +1,84 @@
 import { prisma } from "@/prisma";
+import { FieldTypes } from "@/types";
 import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const key = req.query.key as string;
+  const submission: {
+    questions: {
+      questionId: string;
+      answerText?: string;
+      answerChoices?: string[];
+      type: FieldTypes;
+    }[];
+  } = {
+    questions: [
+      {
+        questionId: "3cf1e6dc-662f-416c-a032-9707f80979a8",
+        answerText: "Akinkunmi",
+        type: "short_text",
+      },
+      {
+        questionId: "a97122ed-b20a-4460-aebc-a55d4adba10c",
+        answerText:
+          "I'm a software enginner with 5 years of experience building developer and productivity tools",
+        type: "long_text",
+      },
+      {
+        questionId: "53f61714-031b-4571-b7a3-a40b5a74e243",
+        answerChoices: ["Dev tools", "SaaS"],
+        type: "multiple_choice",
+      },
+      {
+        questionId: "17b17aeb-a092-4ed9-8503-90f77fc81305",
+        answerText: "Uploadfly",
+        type: "short_text",
+      },
+      {
+        questionId: "4ef4d093-c53b-4113-858d-af8cd30bc01c",
+        answerText:
+          "Uploadfly makes it easy for developers to add file uploads to their apps",
+        type: "long_text",
+      },
+      {
+        questionId: "97281895-cd4e-4bca-94d6-4468d360fd7e",
+        answerChoices: ["$0 - $100k"],
+        type: "multiple_choice",
+      },
+    ],
+  };
 
-  const submissionData: {
-    type: "short-text" | "long-text" | "multiple-choice";
-    questionId: string;
-    answer: any;
-  }[] = [
-    {
-      type: "short-text",
-      questionId: "864ad700-fd60-49b9-988b-bbd3c35a0225",
-      answer: "xing",
-    },
-    {
-      type: "long-text",
-      questionId: "f5e82b24-c846-4e88-abed-48d1b8fc8ff9",
-      answer:
-        "Software engineer with a passion for building software that improves the lives of those around it. I enjoy solving problems and building products that have a positive impact on people.",
-    },
-    {
-      type: "short-text",
-      questionId: "261b6861-52d9-4f70-856b-7530a39ec75d",
-      answer: "21",
-    },
-    {
-      type: "multiple-choice",
-      questionId: "2d768c0d-9df8-422b-96d4-70ea9a050ce6",
-      answer: ["Gaming", "Coding"],
-    },
-  ];
+  const form = await prisma.forms.findUnique({
+    where: { key },
+  });
 
   try {
-    const form = await prisma.forms.findUnique({
-      where: {
-        key,
-      },
-    });
-    if (!form) {
-      res.status(404).json({
-        message: "Form does not exist",
-      });
-    }
-    const newSubmission = await prisma.submissions.create({
+    const createdSubmission = await prisma.submissions.create({
       data: {
-        formId: form?.id as string,
-        userId: form?.userId as string,
+        form: {
+          connect: { id: form?.id },
+        },
+        answers: {
+          create: submission.questions.map((question) => {
+            const answerChoices =
+              question.type === "multiple_choice"
+                ? question.answerChoices
+                : undefined;
+
+            return {
+              question: { connect: { id: question.questionId } },
+              answerText: question.answerText,
+              answerChoices: answerChoices,
+            };
+          }),
+        },
       },
     });
-
-    for (const answer of submissionData) {
-      switch (answer.type) {
-        case "short-text":
-          await prisma.shortTextSubmissions.create({
-            data: {
-              answer: answer.answer,
-              questionId: answer.questionId,
-              submissionId: newSubmission.id,
-            },
-          });
-          break;
-        case "long-text":
-          await prisma.longTextSubmissions.create({
-            data: {
-              answer: answer.answer,
-              questionId: answer.questionId,
-              submissionId: newSubmission.id,
-            },
-          });
-          break;
-        case "multiple-choice":
-          await prisma.multipleChoiceSubmissions.create({
-            data: {
-              answer: answer.answer,
-              questionId: answer.questionId,
-              submissionId: newSubmission.id,
-            },
-          });
-      }
-    }
-    res.send("Submitted");
+    res.send(createdSubmission);
   } catch (error) {
-    res.status(500).json({
-      message: "Something went wrong",
-    });
+    console.log(error);
+    res.send("Something went wrong");
   }
 };
 export default allowMethods(["GET"])(handler);
