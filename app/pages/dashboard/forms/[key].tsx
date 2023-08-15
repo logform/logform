0648abe800dc09176c0c7a5472f49b74bff0e949
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-15 02:27:29 +0100

    ???

--- app/pages/dashboard/forms/[key].tsx
+++
@@ -1,7 +0,0 @@
-import FormLayout from "@/layouts/FormLayout";
-
-const FormPage = () => {
-  return <FormLayout>FormPage</FormLayout>;
-};
-
-export default FormPage;
--- app/pages/dashboard/settings.tsx
+++
@@ -1,8 +0,0 @@
-import DashboardLayout from "@/layouts/DashboardLayout";
-import React from "react";
-
-const Settings = () => {
-  return <DashboardLayout>Settings</DashboardLayout>;
-};
-
-export default Settings;
