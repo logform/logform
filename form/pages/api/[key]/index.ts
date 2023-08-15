54f1977efc5fb127a22d0c838fb5b2a4e13f7dcf
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-15 11:41:13 +0100

    get form

--- form/pages/api/[key]/index.ts
+++ form/pages/api/[key]/index.ts
@@ -9,84 +9,33 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       where: {
         key,
       },
-      include: {
-        shortTextFields: {
-          orderBy: {
-            index: "asc",
-          },
+      select: {
+        title: true,
+        questions: {
           select: {
             id: true,
+            required: true,
             index: true,
             label: true,
-            maxCharacters: true,
-            required: true,
-          },
-        },
-        longTextsFields: {
-          orderBy: {
-            index: "asc",
-          },
-        },
-        MultipleChoiceFields: {
-          include: {
             options: true,
-          },
-          orderBy: {
-            index: "asc",
+            longText: {
+              select: {
+                maxCharacters: true,
+              },
+            },
+            shortText: {
+              select: {
+                maxCharacters: true,
+              },
+            },
           },
         },
       },
     });
-
-    if (!form) {
-      return res.status(404).json({ error: "Form not found" });
-    }
-
-    const formatted = {
-      questions: form.shortTextFields
-        .map((field) => ({
-          id: field.id,
-          index: field.index,
-          type: "short-text",
-          label: field.label,
-          required: field.required,
-        }))
-        .concat(
-          form.MultipleChoiceFields.map((field) => ({
-            id: field.id,
-            index: field.index,
-            label: field.label,
-            required: field.required,
-            type: "multiple-choice",
-            options: field.options.map((option) => ({
-              index: option.index,
-              value: option.value,
-            })),
-          }))
-        )
-        .concat(
-          form.longTextsFields.map((field) => ({
-            id: field.id,
-            index: field.index,
-            type: "long-text",
-            label: field.label,
-            required: field.required,
-          }))
-        ),
-    };
-
-    const sorted = formatted.questions.sort((a, b) => a.index - b.index);
-
-    const response = {
-      title: form.title,
-      key: form.key,
-      questions: sorted,
-    };
-
-    res.json(response);
-  } catch (err: any) {
-    console.error(err);
-    res.status(500).json({ error: "Something went wrong" });
+    res.json(form);
+  } catch (error) {
+    res.send("Something went wrong");
+    console.log(error);
   }
 };
 
