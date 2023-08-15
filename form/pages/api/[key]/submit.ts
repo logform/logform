6cf6ff97a480ddc1ad47b340a478b19844c0773a
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-15 01:26:45 +0100

    ??

--- form/pages/api/[key]/submit.ts
+++ form/pages/api/[key]/submit.ts
@@ -5,22 +5,31 @@ import { allowMethods } from "next-method-guard";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const key = req.query.key as string;
 
-  const submissionData = [
+  const submissionData: {
+    type: "short-text" | "long-text" | "multiple-choice";
+    questionId: string;
+    answer: any;
+  }[] = [
     {
       type: "short-text",
-      questionId: "e95583cd-2d54-4eeb-843e-ca3c3ca1018b",
-      answer: "Akinkunmi",
+      questionId: "864ad700-fd60-49b9-988b-bbd3c35a0225",
+      answer: "xing",
     },
     {
       type: "long-text",
-      questionId: "1311370d-996e-489b-a024-545cc30eb482",
+      questionId: "f5e82b24-c846-4e88-abed-48d1b8fc8ff9",
       answer:
         "Software engineer with a passion for building software that improves the lives of those around it. I enjoy solving problems and building products that have a positive impact on people.",
     },
     {
+      type: "short-text",
+      questionId: "261b6861-52d9-4f70-856b-7530a39ec75d",
+      answer: "21",
+    },
+    {
       type: "multiple-choice",
-      questionId: "da51189a-8600-42d1-8624-dd3b62a95dab",
-      answer: ["Air", "Water"],
+      questionId: "2d768c0d-9df8-422b-96d4-70ea9a050ce6",
+      answer: ["Gaming", "Coding"],
     },
   ];
 
@@ -41,6 +50,38 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         userId: form?.userId as string,
       },
     });
+
+    for (const answer of submissionData) {
+      switch (answer.type) {
+        case "short-text":
+          await prisma.shortTextSubmissions.create({
+            data: {
+              answer: answer.answer,
+              questionId: answer.questionId,
+              submissionId: newSubmission.id,
+            },
+          });
+          break;
+        case "long-text":
+          await prisma.longTextSubmissions.create({
+            data: {
+              answer: answer.answer,
+              questionId: answer.questionId,
+              submissionId: newSubmission.id,
+            },
+          });
+          break;
+        case "multiple-choice":
+          await prisma.multipleChoiceSubmissions.create({
+            data: {
+              answer: answer.answer,
+              questionId: answer.questionId,
+              submissionId: newSubmission.id,
+            },
+          });
+      }
+    }
+    res.send("Submitted");
   } catch (error) {
     res.status(500).json({
       message: "Something went wrong",
