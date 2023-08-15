62286b28a065e4c1a1d9a9f049bbf433b58237c0
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-15 13:08:06 +0100

    fetch summary

--- app/pages/forms/[key].tsx
+++ app/pages/forms/[key].tsx
@@ -1,6 +1,24 @@
 import FormLayout from "@/layouts/FormLayout";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const FormPage = () => {
+  const [summary, setSumarry] = useState({});
+
+  const router = useRouter();
+  const { key } = router.query;
+  useEffect(() => {
+    if (key) {
+      (async () => {
+        try {
+          const { data } = await axios(`/api/form/${key}`);
+          console.log(JSON.stringify(data));
+          setSumarry(data);
+        } catch (error) {}
+      })();
+    }
+  }, [router.query.key]);
   return <FormLayout>FormPage</FormLayout>;
 };
 
