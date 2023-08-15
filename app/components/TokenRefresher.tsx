5099eb2a701443d12fe5c266083630e6e3ff9234
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-15 13:07:22 +0100

    global token refresh

--- app/components/TokenRefresher.tsx
+++ app/components/TokenRefresher.tsx
@@ -1,19 +1,25 @@
 import axios from "axios";
 import useInterval from "use-interval";
 import { ReactNode } from "react";
+import { useRouter } from "next/router";
 
 const TokenRefresher = ({ children }: { children: ReactNode }) => {
-  useInterval(
-    async () => {
-      try {
-        await axios("/api/auth/refresh-token");
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    840000,
-    true
-  );
+  const router = useRouter();
+
+  if (router.asPath !== "/login" && router.asPath !== "/signup") {
+    useInterval(
+      async () => {
+        try {
+          await axios("/api/auth/refresh-token");
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      840000,
+      true
+    );
+  }
+
   return <>{children}</>;
 };
 
