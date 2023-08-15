1507fecf4a8655baf4e84ed90ffea16870321c77
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-15 11:10:03 +0100

    yayyy!!!

--- app/pages/api/form/create.ts
+++ app/pages/api/form/create.ts
@@ -6,91 +6,103 @@ import { NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
 const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
-  const form: { title: string; questions: QuestionProps[] } = {
-    title: "Test form",
+  const formPayload = {
+    title: "Logfund",
     questions: [
       {
         index: 1,
-        type: "short-text",
-        label: "Name",
+        type: "short_text",
+        label: "What's your name?",
         required: true,
         maxCharacters: 100,
       },
-      { index: 2, label: "About", required: true, type: "long-text" },
-      { index: 3, label: "Age", required: true, type: "short-text" },
+      {
+        index: 2,
+        label: "Tell us about yourself",
+        required: true,
+        type: "long_text",
+        maxCharacters: 400,
+      },
+      {
+        index: 3,
+        label: "What field is your startup in?",
+        required: false,
+        type: "multiple_choice",
+        options: [
+          { index: 1, value: "Dev tools" },
+          { index: 2, value: "SaaS" },
+          { index: 3, value: "BaaS" },
+          { index: 4, value: "Infrastructure" },
+        ],
+      },
       {
         index: 4,
-        label: "Interests",
+        label: "What's your startup name?",
+        required: false,
+        type: "short_text",
+      },
+      {
+        index: 5,
+        label: "Tell us about your startup",
         required: true,
-        type: "multiple-choice",
+        type: "long_text",
+        maxCharacters: 300,
+      },
+      {
+        index: 6,
+        label: "How much have you raised?",
+        required: false,
+        type: "multiple_choice",
         options: [
-          { index: 1, value: "Gaming" },
-          { index: 2, value: "Coding" },
-          { index: 3, value: "Fishing" },
-          { index: 4, value: "Hiking" },
+          { index: 1, value: "$0 - $100k" },
+          { index: 2, value: "$100k - $500k" },
+          { index: 3, value: "$500k - $1m" },
+          { index: 4, value: "$1m+" },
         ],
       },
     ],
   };
 
   try {
-    const newForm = await prisma.forms.create({
+    const createdForm = await prisma.forms.create({
       data: {
-        title: form.title,
-        userId: req.userId,
+        title: formPayload.title,
         key: generateKey(6),
+        userId: req.userId,
+        questions: {
+          create: formPayload.questions.map((question) => ({
+            index: question.index,
+            label: question.label,
+            type: question.type as QuestionProps["type"],
+            required: question.required,
+            options: {
+              create:
+                question.options &&
+                question.options.map((option) => ({
+                  index: option.index,
+                  value: option.value,
+                })),
+            },
+            shortText:
+              question.type === "short_text"
+                ? {
+                    create: { maxCharacters: question.maxCharacters },
+                  }
+                : undefined,
+            longText:
+              question.type === "long_text"
+                ? {
+                    create: { maxCharacters: question.maxCharacters },
+                  }
+                : undefined,
+          })),
+        },
       },
     });
-
-    for (const question of form.questions) {
-      switch (question.type) {
-        case "short-text":
-          await prisma.shortTextsFields.create({
-            data: {
-              formId: newForm.id,
-              label: question.label,
-              index: question.index,
-              required: question.required,
-              maxCharacters: question?.maxCharacters,
-            },
-          });
-          break;
-        case "long-text":
-          await prisma.longTextsFields.create({
-            data: {
-              formId: newForm.id,
-              index: question.index,
-              label: question.label,
-              required: question.required,
-              maxCharacters: question?.maxCharacters,
-            },
-          });
-          break;
-        case "multiple-choice":
-          const newMultipleChoice = await prisma.multipleChoiceFields.create({
-            data: {
-              formId: newForm.id,
-              index: question.index,
-              label: question.label,
-              required: question.required,
-            },
-          });
-          question.options.map(async (option) => {
-            await prisma.multipleChoiceFieldsOptions.create({
-              data: {
-                index: option.index,
-                value: option.value,
-                questionId: newMultipleChoice.id,
-              },
-            });
-          });
-          break;
-      }
-    }
-    res.send("Form created");
-  } catch (err: any) {
-    console.log(err);
-    res.status(500).send("Something went wrong");
+    res.send(createdForm);
+  } catch (error) {
+    res.send("Something went wrong");
+    console.log(error);
   }
 };
 
