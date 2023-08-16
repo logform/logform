beb9450efa21be35bf77dd6a29b53949bff1f33a
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-16 12:03:41 +0100

    percentage shit

--- app/pages/forms/[key].tsx
+++ app/pages/forms/[key].tsx
@@ -22,22 +22,45 @@ const FormPage = () => {
   return (
     <FormLayout>
       <div className="flex items-start w-[60%] mx-auto flex-col py-5 gap-5">
-        {summary?.questionSummaries.map((questionSummary, i) => (
+        {summary?.questionSummaries.map((response, i) => (
           <div
             className="border-2 border-gray-300 w-full p-5 rounded-md"
             key={i}
           >
-            <h1 className="font-semibold">{questionSummary.questionLabel}</h1>
+            <h1 className="font-semibold">{response?.questionLabel}</h1>
             <p className="text-sm">
-              {questionSummary.answerCount} response
-              {questionSummary.answerCount === 1 ? "" : "s"}
+              {response.answerCount} response
+              {response.answerCount === 1 ? "" : "s"}
             </p>
             <div className="mt-3">
-              {questionSummary.latestAnswers.map((answer, i) => (
-                <div className="border-b my-5 border-gray-300" key={i}>
-                  <p>{answer}</p>
-                </div>
-              ))}
+              {response.responses.latestAnswers &&
+                response.responses.latestAnswers.map((answer, i) => (
+                  <div className="border-b my-5 border-gray-300" key={i}>
+                    <p>{answer}</p>
+                  </div>
+                ))}
+              {response.responses.optionSummaries &&
+                response.responses.optionSummaries.map((answer, i) => (
+                  <div
+                    className="w-full h-10 bg-gray-200/20 font-semibold border my-5 rounded-md relative"
+                    key={i}
+                  >
+                    <div className="flex items-center justify-between h-full px-2">
+                      <p>{answer.option}</p>
+                      <p>
+                        {answer.percentage}% •{" "}
+                        <span className="text-sm">
+                          {answer.count} response
+                          {answer.count === 1 ? "" : "s"}
+                        </span>
+                      </p>
+                    </div>
+                    <div
+                      className="h-full bg-black/20 rounded-md absolute top-0 left-0"
+                      style={{ width: `${answer.percentage}%` }}
+                    ></div>
+                  </div>
+                ))}
             </div>
           </div>
         ))}
