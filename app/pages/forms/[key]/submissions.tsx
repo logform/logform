26a9100340cda288d27b06488e538fbf3a802a96
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-17 20:27:59 +0100

    initial commit

---
+++ app/pages/forms/[key]/submissions.tsx
@@ -0,0 +1,8 @@
+import FormLayout from "@/layouts/FormLayout";
+import React from "react";
+
+const Submissions = () => {
+  return <FormLayout pageName="Submissions">Submissions</FormLayout>;
+};
+
+export default Submissions;
