f6c06614c30c551044dd7d5ec9991f25f0a6a095
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-17 21:40:13 +0100

    get submissions

---
+++ app/pages/api/form/[key]/submissions.ts
@@ -0,0 +1,53 @@
+import { ExtendedRequest } from "@/interfaces";
+import { validateAccessToken } from "@/middlewares/validateAccessToken";
+import { prisma } from "@/prisma";
+import { NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+
+const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
+  const key = req.query.key;
+
+  const form = await prisma.forms.findUnique({
+    where: {
+      key: key as string,
+    },
+    select: {
+      questions: {
+        select: {
+          label: true,
+          type: true,
+          index: true,
+        },
+        orderBy: {
+          index: "asc",
+        },
+      },
+      submissions: {
+        select: {
+          id: true,
+          formId: true,
+          submittedAt: true,
+          answers: {
+            select: {
+              id: true,
+              answerText: true,
+              answerChoices: true,
+              questionId: true,
+              submissionId: true,
+            },
+            orderBy: {
+              questionId: "asc",
+            },
+          },
+        },
+      },
+    },
+  });
+
+  res.json(form);
+};
+
+export default allowMethods(["GET"])(
+  (req: ExtendedRequest, res: NextApiResponse) =>
+    validateAccessToken(req, res, () => handler(req, res))
+);
