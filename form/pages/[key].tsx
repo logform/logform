7c2485f3c54a2f572fc7b05a70956026d2a2f82f
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-18 17:26:18 +0100

    handle change and next

--- form/pages/[key].tsx
+++ form/pages/[key].tsx
@@ -1,20 +1,126 @@
-import { QuestionProps } from "@/types";
+import LongText from "@/components/LongText";
+import MultipleChoice from "@/components/MultipleChoice";
+import ShortText from "@/components/ShortText";
+import { FieldTypes, QuestionProps } from "@/types";
 import axios from "axios";
 import type { GetServerSideProps } from "next";
 import Head from "next/head";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const Form = ({
   form,
 }: {
-  form: { title: string; key: string; questions: QuestionProps };
+  form: { title: string; key: string; questions: QuestionProps[] };
 }) => {
+  const [currentQuestion, setCurrentQuestion] = useState<{
+    index: number;
+    required: boolean;
+    value: any;
+  }>({
+    index: form.questions[0].index,
+    required: form.questions[0].required,
+    value: null,
+  });
+
+  const [answers, setAnswers] = useState<
+    {
+      questionId: string;
+      answerText?: string;
+      answerChoices?: string[];
+      type: FieldTypes;
+      questionIndex: number;
+    }[]
+  >([]);
+
+  const handleNext = () => {
+    if (currentQuestion.required && !currentQuestion.value) {
+      toast("This question is required");
+      return;
+    }
+
+    const currentQuestionIndex = form.questions.findIndex(
+      (question) => question.index === currentQuestion.index
+    );
+
+    if (currentQuestionIndex >= 0) {
+      const updatedAnswers = [...answers];
+      updatedAnswers[currentQuestionIndex] = {
+        questionId: form.questions[currentQuestionIndex].id,
+        type: "short_text",
+        questionIndex: currentQuestion.index,
+        answerText: currentQuestion.value,
+      };
+
+      const nextQuestion = form.questions[currentQuestionIndex + 1];
+      if (nextQuestion) {
+        setCurrentQuestion({
+          index: nextQuestion.index,
+          required: nextQuestion.required,
+          value: null,
+        });
+        setAnswers(updatedAnswers);
+      }
+    }
+  };
+
   return (
-    <div>
+    <div className="">
+      <div className="w-full h-40 bg-pink-400 mb-10 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-center mb-10">{form.title}</h1>
+      </div>
       <Head>
         <title>{form.title}</title>
       </Head>
-      <h1 className="text-3xl font-bold underline text-center">{form.title}</h1>
-      <p>{JSON.stringify(form.questions)}</p>
+
+      <div className="flex flex-col text-lg items-center w-[60%] gap-2 mx-auto font-semibold">
+        {form.questions.map((question) => (
+          <div className="w-[450px]">
+            {question.index === currentQuestion.index && (
+              <p className="mb-2">{question.label}</p>
+            )}
+            {question.type === "short_text" &&
+              question.index === currentQuestion.index && (
+                <ShortText
+                  onChange={(e) => {
+                    setCurrentQuestion({
+                      ...currentQuestion,
+                      value: e.target?.value,
+                    });
+                  }}
+                />
+              )}
+            {question.type === "long_text" &&
+              question.index === currentQuestion.index && (
+                <LongText
+                  onChange={(e) => {
+                    setCurrentQuestion({
+                      ...currentQuestion,
+                      value: e.target?.value,
+                    });
+                  }}
+                />
+              )}
+            {/* {question.type === "multiple_choice" && (
+              <MultipleChoice options={question.options} />
+            )} */}
+          </div>
+        ))}
+        <button
+          className="outline-none border-2 text-white bg-black/90 hover:bg-black transition-colors rounded-full pl-3 w-[450px] py-3 font-semibold text-center flex items-center justify-center disabled:cursor-not-allowed h-12"
+          onClick={handleNext}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => {
+            console.log(answers);
+            console.log(currentQuestion.value);
+          }}
+        >
+          Check
+        </button>
+      </div>
     </div>
   );
 };
