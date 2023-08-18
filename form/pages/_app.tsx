fb93d6d0ab0ab97506427c1f8a0eddcd018f0757
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-18 17:26:05 +0100

    global render <ToastContainer/>

--- form/pages/_app.tsx
+++ form/pages/_app.tsx
@@ -1,9 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <ToastContainer />
       <Component {...pageProps} />
     </>
   );
