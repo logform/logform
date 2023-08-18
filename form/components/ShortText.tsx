e3be054a867460b97fc04a7adee78441b89235c5
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-18 17:25:47 +0100

    added onChange event

--- form/components/ShortText.tsx
+++ form/components/ShortText.tsx
@@ -1,5 +1,19 @@
-const ShortText = () => {
-  return <div>ShortText</div>;
+import { ChangeEvent } from "react";
+
+const ShortText = ({
+  onChange,
+}: {
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}) => {
+  return (
+    <div>
+      <input
+        type="text"
+        className="outline-none border border-gray-400 rounded-full pl-3 w-full py-2 font-semibold focus:border-black transition-colors"
+        onChange={onChange}
+      />
+    </div>
+  );
 };
 
 export default ShortText;
