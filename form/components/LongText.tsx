ad6fe24474049339f63bb77d2a1c3e9e60c2cfa5
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-18 17:25:21 +0100

    onchange

--- form/components/LongText.tsx
+++ form/components/LongText.tsx
@@ -1,5 +1,18 @@
-const LongText = () => {
-  return <div>LongText</div>;
+import { ChangeEvent } from "react";
+
+const LongText = ({
+  onChange,
+}: {
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+}) => {
+  return (
+    <div>
+      <textarea
+        className="outline-none border border-gray-400 rounded-md text-lg pl-3 py-1 focus:border-black w-full h-[200px]"
+        onChange={onChange}
+      />
+    </div>
+  );
 };
 
 export default LongText;
